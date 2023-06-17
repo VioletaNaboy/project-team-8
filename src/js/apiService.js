26dc9e931ee186c502ea7ffaf3d4c4eb3e53440e
Danylo Tytarenko <danyatitar14@gmail.com>
2023-06-17 11:16:48 +0300

    add apiService

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,1 +1,40 @@
-// Create new file
+import axios from 'axios';
+
+const apiUrl = 'https://books-backend.p.goit.global/books';
+
+async function getCategories() {
+  try {
+    const result = await axios.get(`${apiUrl}/category-list`);
+    return result;
+  } catch (e) {
+    return e;
+  }
+}
+
+async function getTopBooks() {
+  try {
+    const result = await axios.get(`${apiUrl}/top-books`);
+    return result;
+  } catch (e) {
+    return e;
+  }
+}
+
+async function getBooksByCategory(category) {
+  try {
+    const result = await axios.get(`${apiUrl}/category?category=${category}`);
+    return result;
+  } catch (e) {
+    return e;
+  }
+}
+async function getBookById(id) {
+  try {
+    const result = await axios.get(`${apiUrl}/${id}`);
+    return result;
+  } catch (e) {
+    return e;
+  }
+}
+
+export { getCategories, getTopBooks, getBooksByCategory, getBookById };
