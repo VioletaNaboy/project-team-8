3b6c67e60a81bd8b108a0a931cb6f8f8179f1dd8
Alex250182 <119724458+Alex250182@users.noreply.github.com>
2023-06-18 17:32:12 +0300

    add

--- src/js/render-category-list.js
+++ src/js/render-category-list.js
@@ -1,8 +1,14 @@
+<<<<<<< Updated upstream
 import { getCategories } from './apiService.';
+=======
+ 
+export { getCategories } from './apiService.';
+>>>>>>> Stashed changes
 
 const categoryList = document.querySelector('category__list');
 
 
+<<<<<<< Updated upstream
 getCategories()
   .then(categories => {
     renderCategoryList(categories);
@@ -23,3 +29,30 @@ function renderCategoryList(categories) {
   
 }
 
+=======
+  getCategories()
+  .then(categories => {
+    renderCategoryList(categories);
+  })
+  .catch(error => {
+        console.log(error);
+      });
+
+
+function renderCategoryList(categories) {
+
+    categoryList.innerHTML = categories.map(category => {
+        return `
+         <li class="category_list" name = "${category.list_name}">
+         ${category.list_name}
+         </li>`;
+    }).join('');
+    
+    
+}
+  
+
+
+
+  
+>>>>>>> Stashed changes
