d8f293697ed430fbe35f78ef563041be7106c771
VioletaNaboy <vnaboishchykova@gmail.com>
2023-06-18 11:53:34 +0200

    fixed_src

--- src/js/shopping-list-card.js
+++ src/js/shopping-list-card.js
@@ -37,20 +37,20 @@ export default function createMarkupLibraryCard({
                 <picture class="shop-icone" width="32" height="11">
                   <source
                     srcset="
-                      ./img/shopping-list/icon-amazon-desktop.png,
-                      ./img/shopping-list/icon-amazon-desktop@2x.png
+                      ../img/shopping-list/icon-amazon-desktop.png,
+                      ../img/shopping-list/icon-amazon-desktop@2x.png
                     "
                     media="(min-width: 768px)"
                   />
                   <source
                     srcset="
-                      ./img/shopping-list/icon-amazon-mobile.png,
-                      ./img/shopping-list/icon-amazon-mobile@2x.png
+                      ../img/shopping-list/icon-amazon-mobile.png,
+                      ../img/shopping-list/icon-amazon-mobile@2x.png
                     "
                     media="(max-width: 767px)"
                   />
                   <img
-                    src="./img/shopping-list/icon-amazon-desktop.png, ./img/shopping-list/icon-amazon-desktop@2x.png"
+                    src="../img/shopping-list/icon-amazon-desktop.png, ../img/shopping-list/icon-amazon-desktop@2x.png"
                     alt="icon amazon"
                   />
                 </picture>
@@ -61,20 +61,20 @@ export default function createMarkupLibraryCard({
                 <picture class="shop-icone" width="16" height="16">
                   <source
                     srcset="
-                      ./img/shopping-list/icon-apple-books-desktop.png,
-                      ./img/shopping-list/icon-apple-books-desktop@2x.png
+                      ../img/shopping-list/icon-apple-books-desktop.png,
+                      ../img/shopping-list/icon-apple-books-desktop@2x.png
                     "
                     media="(min-width: 768px)"
                   />
                   <source
                     srcset="
-                      ./img/shopping-list/icon-apple-books-mobile.png,
-                      ./img/shopping-list/icon-apple-books-mobile@2x.png
+                      ../img/shopping-list/icon-apple-books-mobile.png,
+                      ../img/shopping-list/icon-apple-books-mobile@2x.png
                     "
                     media="(max-width: 767px)"
                   />
                   <img
-                    src="./img/shopping-list/icon-apple-books-desktop.png, ./img/shopping-list/icon-apple-books-desktop@2x.png"
+                    src="../img/shopping-list/icon-apple-books-desktop.png, ../img/shopping-list/icon-apple-books-desktop@2x.png"
                     alt="icon apple books"
                   />
                 </picture>
@@ -85,20 +85,20 @@ export default function createMarkupLibraryCard({
                  <picture class="shop-icone" width="16" height="16">
                   <source
                     srcset="
-                      ./img/shopping-list/icon-book-shop-desktop.png,
-                      ./img/shopping-list/icon-book-shop-desktop@2x.png
+                      ../img/shopping-list/icon-book-shop-desktop.png,
+                      ../img/shopping-list/icon-book-shop-desktop@2x.png
                     "
                     media="(min-width: 768px)"
                   />
                   <source
                     srcset="
-                      ./img/shopping-list/icon-book-shop-mobile.png,
-                      ./img/shopping-list/icon-book-shop-mobile@2x.png
+                      ../img/shopping-list/icon-book-shop-mobile.png,
+                      ../img/shopping-list/icon-book-shop-mobile@2x.png
                     "
                     media="(max-width: 767px)"
                   />
                   <img
-                    src="./img/shopping-list/icon-book-shop-desktop.png, ./img/shopping-list/icon-book-shop-desktop@2x.png"
+                    src="../img/shopping-list/icon-book-shop-desktop.png, ../img/shopping-list/icon-book-shop-desktop@2x.png"
                     alt="icon book shop"
                   />
                 </picture>
