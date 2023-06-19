f02250d1b403c8921dc06da2c6735f02ec895808
Babur1992 <jon1992@ukr.net>
2023-06-19 11:17:40 +0300

    maodalAvtorisation

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ import './js/support.js';
 import './js/render-category-list.js';
 import './js/shopping-list-card';
 import './js/render-shopping-list';
+import { closeModal, openModal } from './js/modal';
+closeModal('closeModal ');
+openModal('openModal');
