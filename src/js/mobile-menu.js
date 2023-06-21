9c4ec4d81beb10ae424771eb2f66004b39e0abc8
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-06-22 01:42:10 +0300

    update "account out"

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,17 +1,26 @@
 const mobileMenuOpenIconEl = document.querySelector('.mobile-menu__open-icon');
 
 if (mobileMenuOpenIconEl) {
-  const mobileMenuAccountInEl = document.querySelector(
-    '.mobile-menu-container__account-in'
-  );
-  const mobileMenuContainerEl = document.querySelector(
+  // Користувач залогінений
+  const mobileMenuContainerInEl = document.querySelector(
     '.mobile-menu-container__in'
   );
+  // ! ---------
+
+  // Користувач НЕ залогінений
+  const mobileMenuContainerOutEl = document.querySelector(
+    '.mobile-menu-container__out'
+  );
+  // ! ---------
 
   mobileMenuOpenIconEl.addEventListener('click', function (e) {
     document.body.classList.toggle('_lock');
     mobileMenuOpenIconEl.classList.toggle('_active-icon');
-    mobileMenuAccountInEl.classList.toggle('_active-icon');
-    mobileMenuContainerEl.classList.toggle('_active-menu');
+    // mobileMenuAccountInEl.classList.toggle('_active-icon');
+    mobileMenuContainerInEl.classList.toggle('_active-menu');
   });
 }
+
+// Користувач НЕ залогінений
+// ! Релізація через перевірку "авторизація"
+// mobileMenuContainerOutEl.classList.toggle('_active-menu');
