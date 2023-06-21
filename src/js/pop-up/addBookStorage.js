1cd55be8e25e35ac69a63c673b61ce5a0950102c
Inna-Kulish <norelajest@gmail.com>
2023-06-21 18:40:40 +0300

    animation

--- src/js/pop-up/addBookStorage.js
+++ src/js/pop-up/addBookStorage.js
@@ -2,8 +2,9 @@ import { saveToLocalStoradge, loadFromLocalStoradge } from '../localStorageApi';
 import { getArrBookId } from './getDataBooksById';
 
 const addBookBtn = document.querySelector('.pop-up-modal-btn');
+const removeTxt = document.querySelector('.pop-up-remove-txt');
 addBookBtn.addEventListener('click', onAddBtnClick);
-
+// Сongratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.
 export default class StorageBooks {
   constructor() {
     this.key = 'shopping-list';
@@ -15,9 +16,7 @@ export default class StorageBooks {
   }
 
   getArrayIds() {
-this.arrayIds = this.objectBooks.map(
-  bookId => bookId._id
-); 
+    this.arrayIds = this.objectBooks.map(bookId => bookId._id);
   }
 
   setCurrentBookId() {
@@ -25,47 +24,44 @@ this.arrayIds = this.objectBooks.map(
   }
 
   getCurrentIds() {
-    this.arrayIds = this.savedObject.map(
-      bookId => bookId._id
-    ); 
+    this.arrayIds = this.savedObject.map(bookId => bookId._id);
   }
 }
 
 const storageBooks = new StorageBooks();
 
 function checkBook(id) {
-  addBookBtn.textContent = 'Add to shopping list';
+  switchAddBtn();
   saveToLocalStoradge(storageBooks.currentBookKey, id);
 
-if(storageBooks.savedObject) {
-  storageBooks.objectBooks = storageBooks.savedObject;
-  storageBooks.getCurrentIds();
-}
+  if (storageBooks.savedObject) {
+    storageBooks.objectBooks = storageBooks.savedObject;
+    storageBooks.getCurrentIds();
+  }
 
   storageBooks.getArrayIds();
-
   if (storageBooks.arrayIds.includes(id)) {
-    addBookBtn.textContent = 'remove from the shopping list';
+    switchRemoveBtn();
   }
 }
 
 async function onAddBtnClick() {
-  storageBooks.setCurrentBookId()
+  storageBooks.setCurrentBookId();
 
-  if(storageBooks.savedObject) {
+  if (storageBooks.savedObject) {
     storageBooks.objectBooks = storageBooks.savedObject;
     storageBooks.getCurrentIds();
   }
 
   if (storageBooks.arrayIds.includes(storageBooks.currentBookId)) {
-     removeBtnClick();
+    removeBtnClick();
   } else {
     const objectBook = await getArrBookId(storageBooks.currentBookId);
 
     storageBooks.objectBooks.push(objectBook);
     saveToLocalStoradge(storageBooks.key, storageBooks.objectBooks);
 
-    addBookBtn.textContent = 'remove from the shopping list';
+    switchRemoveBtn();
   }
 }
 
@@ -74,7 +70,18 @@ function removeBtnClick() {
 
   storageBooks.objectBooks.splice(indexBook, 1);
   saveToLocalStoradge(storageBooks.key, storageBooks.objectBooks);
+  switchAddBtn();
+}
+
+function switchAddBtn() {
   addBookBtn.textContent = 'Add to shopping list';
+  removeTxt.textContent = '';
+}
+
+function switchRemoveBtn() {
+  addBookBtn.textContent = 'remove from the shopping list';
+  removeTxt.textContent =
+    'Сongratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.';
 }
 
 export { checkBook };
