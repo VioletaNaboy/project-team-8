630c6e92055f61d64835fa2971d0f9fadcb4a61d
VioletaNaboy <vnaboishchykova@gmail.com>
2023-06-21 21:45:21 +0200

    open-modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,14 @@
 const modal = document.querySelector('.modal');
 const closeButton = modal.querySelector('.icon-close');
+const signUpHeader = document.querySelector('.sign-up-header');
 
 export function closeModal() {
-  modal.style.display = 'none';
+  modal.classList.add('hidden');
 }
-
+function openModal() {
+  modal.classList.remove('hidden');
+}
+signUpHeader.addEventListener('click', openModal);
 closeButton.addEventListener('click', closeModal);
 
 window.addEventListener('click', function (event) {
@@ -12,6 +16,3 @@ window.addEventListener('click', function (event) {
     closeModal();
   }
 });
-
-
-
