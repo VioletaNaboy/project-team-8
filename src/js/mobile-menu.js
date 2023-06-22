3e5c95395cc498803352e3c10d5248be36069d62
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-06-22 03:26:11 +0300

    final update "mobile menu"

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -16,11 +16,20 @@ if (mobileMenuOpenIconEl) {
   mobileMenuOpenIconEl.addEventListener('click', function (e) {
     document.body.classList.toggle('_lock');
     mobileMenuOpenIconEl.classList.toggle('_active-icon');
-    // mobileMenuAccountInEl.classList.toggle('_active-icon');
+
     mobileMenuContainerInEl.classList.toggle('_active-menu');
   });
 }
+// ! Логіка додавання і прибирання стилю transition
+//   if (mobileMenuContainerInEl.classList.contains('._active-menu')) {
+//     mobileMenuContainerInEl.style.transition =
+//       'transform 500ms cubic-bezier(0.4, 0, 0.2, 1)';
+//   } else {
+//     mobileMenuContainerInEl.style.transition = '';
+// }
+// ! ---------
 
 // Користувач НЕ залогінений
 // ! Релізація через перевірку "авторизація"
 // mobileMenuContainerOutEl.classList.toggle('_active-menu');
+// ! ---------
