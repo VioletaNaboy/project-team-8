d435cb75490c90b721a1c57e996e4a7908ccdc69
VioletaNaboy <vnaboishchykova@gmail.com>
2023-06-23 16:49:57 +0200

    fixed_js_css_modal-avtr

--- src/js/modal-avtorization.js
+++ src/js/modal-avtorization.js
@@ -1,10 +1,17 @@
-const modalAvtrOpenBtnEl = document.querySelectorAll('.modal-avtr__open-btn');
+const modalAvtrOpenBtnEls = document.querySelectorAll('.modal-avtr__open-btn');
 
-if (modalAvtrOpenBtnEl) {
+if (modalAvtrOpenBtnEls.length > 0) {
   const modalAvtrContainerEl = document.querySelector('.modal-avtr');
-
-  modalAvtrOpenBtnEl.addEventListener('click', function (e) {
-    document.body.classList.toggle('_lock');
-    mobileMenuContainerInEl.classList.toggle('_active-modal');
+  const modalAvtrCloseBtnEls = document.querySelector(
+    '.modal-avtr__form-icon-close'
+  );
+  modalAvtrOpenBtnEls.forEach(function (modalAvtrOpenBtnEl) {
+    modalAvtrOpenBtnEl.addEventListener('click', function (e) {
+      document.body.classList.toggle('_lock');
+      modalAvtrContainerEl.classList.toggle('_active-modal');
+    });
+  });
+  modalAvtrCloseBtnEls.addEventListener('click', () => {
+    modalAvtrContainerEl.classList.remove('_active-modal');
   });
 }
