56e63d889aae995d48160bd1355258850d02798d
Rostykfps <rostikfps@gmail.com>
2023-06-27 13:56:32 +0300

    Changed pagination.js, render-shopping-list.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -8,41 +8,24 @@ const paginationContainerEl = document.getElementById(
 );
 const container = document.getElementById('tui-pagination-container');
 
-let startItemIndex = 0;
-
 export function createPagination() {
   const shoppingList = loadFromLocalStoradge('shopping-list');
-  //   console.log('hhh', shoppingList);
-  //   console.log(currentPage);
-  //   renderShoppingList(shoppingList);
 
+  if (shoppingList.length % 3 === 0) {
+    currentPage = Math.ceil(shoppingList.length / 3);
+  }
+
+  let startItemIndex = currentPage * 3 - 3;
   let dataToRenderShoppingList = shoppingList.slice(
     startItemIndex,
     startItemIndex + 3
   );
-  //   let dataToRenderShoppingList;
-  //   if (shoppingList.length % 3 === 0) {
-  //     dataToRenderShoppingList = shoppingList.slice(
-  //       startItemIndex - 3,
-  //       startItemIndex
-  //     );
-  //   } else {
-  //     dataToRenderShoppingList = shoppingList.slice(
-  //       startItemIndex,
-  //       startItemIndex + 3
-  //     );
-  //   }
 
   if (!shoppingList) {
     return;
   } else if (shoppingList.length < 4) {
     paginationContainerEl.classList.add('display-none');
-    return renderShoppingList(dataToRenderShoppingList);
   }
-  //   let dataToRenderShoppingList = shoppingList.slice(
-  //     startItemIndex,
-  //     startItemIndex + 3
-  //   );
 
   const paginationOptions = {
     totalItems: shoppingList.length,
@@ -50,34 +33,20 @@ export function createPagination() {
     visiblePages: 3,
     page: currentPage,
   };
-  //   console.log(shoppingList.length);
-  renderShoppingList(dataToRenderShoppingList);
 
-  //   console.log('test1');
+  renderShoppingList(dataToRenderShoppingList);
 
   const instance = new Pagination(container, paginationOptions);
   instance.setTotalItems(shoppingList.length);
 
-  //   instance.reset();
-  //   console.log('fff', shoppingList.length);
   instance.on('afterMove', pagination);
   function pagination(event) {
-    // if (shoppingList.length < 4) {
-    //   console.log('sll', shoppingList.length);
-    //   return renderShoppingList(shoppingList);
-    // }
-
     startItemIndex = event.page * 3 - 3;
     dataToRenderShoppingList = shoppingList.slice(
       startItemIndex,
       startItemIndex + 3
     );
     currentPage = event.page;
-    // console.log(event.page);
-    // console.log('object', dataToRenderShoppingList);
-
     renderShoppingList(dataToRenderShoppingList);
-
-    // currentPage = event.page;
   }
 }
